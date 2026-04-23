d2405830429925d3f98fb3ba2604a44c5ba0b0f1
Ageev-Danilo <danilaageev09@gmail.com>
2026-04-23 18:17:58 +0200

    ajjsdkj

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,7 +4,7 @@ import { config } from 'dotenv';
 config();
 
 export const env = cleanEnv(process.env, {
-    PORT: num({ default: 8000 }),
+    PORT: num({ default: 3000 }),
     HOST: str({ default: '0.0.0.0' }),
     DATABASE_URL: str(),
     SECRET_KEY: str(),
