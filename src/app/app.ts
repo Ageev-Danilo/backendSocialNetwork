9266d93dca07737f23950af96aa13af43fe71dab
Ageev-Danilo <danilaageev09@gmail.com>
2026-04-23 18:01:54 +0200

    prisma

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import { env } from '../config/env';
 import { appRoutes } from './routes';
 import { errorHandlerMiddleware } from '../middlewares/error-handler.middleware';
+import { mailRouter } from '../modules/mail/mail.router';
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(cors());
 app.use(express.json());
 app.use(appRoutes);
 app.use(errorHandlerMiddleware);
+app.use(mailRouter)
 
 app.listen(env.PORT, env.HOST, () => {
     console.log(`Server running on http://${env.HOST}:${env.PORT}`);
--- src/config/env.ts
+++ src/config/env.ts
@@ -5,7 +5,7 @@ config();
 
 export const env = cleanEnv(process.env, {
     PORT: num({ default: 3000 }),
-    HOST: str({ default: 'localhost' }),
+    HOST: str({ default: '0.0.0.0' }),
     DATABASE_URL: str(),
     SECRET_KEY: str(),
     TOKEN_TTL: str({ default: '7d' }),
