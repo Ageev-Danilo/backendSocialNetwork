43040fb85913f26d3dba15750978cd17ed740e3c
Ageev-Danilo <danilaageev09@gmail.com>
2026-05-17 12:56:43 +0200

    db

--- dist/config/path.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.thumbnailFilesDir = exports.originalFilesDir = exports.uploadDir = void 0;
-const node_path_1 = require("node:path");
-exports.uploadDir = (0, node_path_1.join)(__dirname, '../../media');
-exports.originalFilesDir = (0, node_path_1.join)(exports.uploadDir, './original');
-exports.thumbnailFilesDir = (0, node_path_1.join)(exports.uploadDir, './thumbnail');
--- dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = require("./app/app");
-const settings_routes_1 = require("./modules/settings/settings.routes");
-app_1.app.use('/settings', settings_routes_1.SettingsRoutes);
